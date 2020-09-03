526e125db2497d77d70446b04292984d36f72c2f
Oded Streigold <odedstreigold@Odeds-Mac-mini.local>
2020-09-03 23:19:23 +0300

    removed tiny-process-library test files
    Signed-off-by: Oded Streigold <odedstreigold@Odeds-Mac-mini.local>

--- tiny-process-library/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Volumes/2 TB/data/Source2/tiny-process-library
-# Build directory: /Volumes/2 TB/data/Source2/tiny-process-library/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
